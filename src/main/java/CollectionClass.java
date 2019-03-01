a86ecc5805826963e93b58053119c24a915046b0
JasonD <jason.daniels@zapper.com>
2019-03-01 14:53:21 +0200

    Added List, Set and Map examples

--- src/main/java/CollectionClass.java
+++ src/main/java/CollectionClass.java
@@ -1,17 +1,15 @@
-import com.sun.org.apache.xalan.internal.xsltc.dom.SortingIterator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CollectionClass {
 
 
     public static void main(String[] args) {
         new CollectionClass();
-    }
+            }
 
 
-    private static final String [] names = {"Jason","Amanda","Kate"};
+    private static final String [] names = {"Jason","Amanda","Kate","Jason","Robert","Adam","Amanda"};
 
     public CollectionClass() {
 
@@ -19,11 +17,40 @@ public class CollectionClass {
         for(String name : names)
             list.add(name);
 
-        for(int i = 0; i < list.size();i++)
-            System.out.printf("%s, ", list.get(i));
+        System.out.println("My Collections List of names are: " );
 
+        for(int i = 0; i < list.size();i++)
+            System.out.printf("%s: ", list.get(i));
+
+        uniqueNames(list);
 
     }
+    private void uniqueNames(Collection<String >collection){
+        Set<String> set = new HashSet<>(collection);
+
+        System.out.println("\n\n"+"My unique Set of names are: ");
+
+        for (String uniqueListofNames : set)
+
+            System.out.printf("%s ",uniqueListofNames);
+
+    Map colors  = new HashMap();
+
+    colors.put("Red",5);
+    colors.put("Blue",3);
+    colors.put("Green",10);
+    colors.put("Pink",2);
+
+        System.out.println("\n\n" + "Total number of MAP colors: " + colors.size());
+
+        for(Object key : colors.keySet())
+            System.out.println(key + "-"+ colors.get(key));
+
+        System.out.println();
+
+    }
+
+
 
 
 
