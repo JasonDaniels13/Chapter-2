83c824d3003513604620223f2ad10b9c1484073d
JasonD <jason.daniels@zapper.com>
2019-03-01 12:38:11 +0200

    Collection and List prinitng to console

--- src/main/java/CollectionClass.java
+++ src/main/java/CollectionClass.java
@@ -1,2 +1,34 @@
+import com.sun.org.apache.xalan.internal.xsltc.dom.SortingIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionClass {
+
+
+    public static void main(String[] args) {
+        new CollectionClass();
+    }
+
+
+    private static final String [] names = {"Jason","Amanda","Kate"};
+
+    public CollectionClass() {
+
+        List<String> list = new ArrayList<>();
+        for(String name : names)
+            list.add(name);
+
+        for(int i = 0; i < list.size();i++)
+            System.out.printf("%s, ", list.get(i));
+
+
+    }
+
+
+
+
 }
+
+
+
